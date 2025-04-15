922488d8cdaf084d5e1b3b1e86efeb41bad87fe6
MateoAldazabal <maldazabal@mail.austral.edu.ar>
2025-04-14 22:08:44 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,17 @@
 def leap_year():
-    print("TO DO")
+    año = int(input("Ingrese un año: "))
+
+    if año % 400 == 0:
+    	valor = "bisiesto"
+	
+    elif año % 100 == 0:
+	    valor = "no bisiesto"
+
+    elif año % 4 == 0:
+	    valor = "bisiesto"
+
+    else: 
+	    valor = "no bisiesto"
+
+    print(f'El año {año} es {valor}')
+
